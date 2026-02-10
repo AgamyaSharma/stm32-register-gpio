e978905fcbf5223d5544f187a33852097dba1505
Agamya Sharma <agamya.005@gmail.com>
2026-02-10 16:16:02 +0530

    feat: implement SysTick blocking delay
    The SysTick timer creates a precise delay by counting down from a value based on the formula: LOAD = (Clock Frequency * Time) - 1. For an 8MHz clock, we load the number 7,999,999 into the register to represent exactly one second of real-world time. The code then waits in a loop for the hardware to flip a flag at zero, ensuring the LED blinks with a perfect 1 sec delay.

--- Src/stm32f302r8.h
+++ Src/stm32f302r8.h
@@ -35,9 +35,19 @@ typedef struct {
 	volatile uint32_t CFRG3;
 } RCC_Reg_t;
 
+typedef struct {
+	volatile uint32_t CTRL;
+	volatile uint32_t LOAD;
+	volatile uint32_t VAL;
+	volatile uint32_t CALIB;
+} SYSTICK_Reg_t;
+
 #define GPIOB_REG_ADDR 0x48000400
 #define GPIOB ((GPIO_Reg_Def_t*) GPIOB_REG_ADDR)
 
 #define RCC_REG_ADDR 0x40021000
 #define RCC ((RCC_Reg_t*) RCC_REG_ADDR)
+
+#define SYSTICK_REG_ADDR 0xE000E010
+#define SYSTICK ((SYSTICK_Reg_t*) SYSTICK_REG_ADDR )
 #endif /* STM32F302R8_H_ */
